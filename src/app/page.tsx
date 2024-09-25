11def885355181498c8ae01ef5b3a97328235fe8
riyaz <riyaz3582@gmail.com>
2024-09-25 13:19:17 +0600

    added authentication and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { FaStar } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa6";
 
 import Image from 'next/image'
+import Phone from "./components/Phone";
 export default function Home() {
   const UserImages = ({ src }) => {
     return (
@@ -65,7 +66,11 @@ export default function Home() {
             </div>
           </div>
           <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
-            
+            <div className="relative md:max-w-xl">
+              <img src="/your-image.png" className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden" alt="" />
+              <img src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" alt="" />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg"/>
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
